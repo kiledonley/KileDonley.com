fa0b7374f48c1d0eccd84b5c9831a06a16d7c741
Kile Donley <kiledonley@gmail.com>
2021-03-08 17:29:36 -0600

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Kile Donley</title>
+        <link rel="icon" />
+      </Head>
+
+      <h1>Hello World!</h1>
+      <h2>Under Construction</h2>
+    </div>
+  )
+}
